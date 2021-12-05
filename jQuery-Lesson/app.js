fdf6be9a8147c483167d270eb754482e7c9657e2
P-manBrowm <tachikawa.yuya@outlook.jp>
2021-12-05 21:51:15 +0900

    [Finish] 2-4-4

---
+++ jQuery-Lesson/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('body').html('<h1>Hello jQuery!!</h1>');
+});
+
+$(document).ready(function(){
+  //jQueryプログラムの内容
+});
+
+$(funciton(){
+  // jQueryプログラムの内容
+})
+
+
+
