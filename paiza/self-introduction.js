99c5de00956c2d39b82931c2ebf8aa7e9276e19a
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2022-01-10 10:52:21 +0900

    finish paiza self-introduction

---
+++ paiza/self-introduction.js
@@ -0,0 +1,5 @@
+// https://paiza.jp/works/mondai/self_introduction/self_introduction__1
+
+console.log("name: P-man Brown");
+console.log("language: JavaScript");
+console.log("hitokoto: Hello World!");
